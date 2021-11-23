e9e34b9acfe37ccec3270f7892628c81f232f1b2
Anto Ark <tuyenngo7bh@gmail.com>
2021-11-23 08:10:51 -0800

    Add project files.

---
+++ Project1/DonorType.h
@@ -0,0 +1,13 @@
+#ifndef DONORTYPE_H
+#define DONORTYPE_H
+
+#include "MemberType.h"
+
+class DonorType : public MemberType
+{
+
+
+};
+
+#endif
+
---
+++ Project1/MemberType.h
@@ -0,0 +1,9 @@
+#ifndef MEMBERTYPE_H
+#define MEMBERTYPE_H
+
+class MemberType
+{
+
+};
+
+#endif
