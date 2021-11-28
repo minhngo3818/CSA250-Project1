d1617030c8a3a6e288068af4f313363294f77f79
Liam Breton <lbreton@student.cccd.edu>
2021-11-28 12:33:27 -0800

    added a part B test file

---
+++ Project1/part B test.cpp
@@ -0,0 +1,41 @@
+/*
+	Night Owls
+
+	Breton, Liam (leader)
+	Ta, Hoa
+	Ngo, Minh
+	Richardson, Nick
+	Riana, Ali
+
+	November 28, 2021
+
+	CSA 250
+	Project 1
+*/
+#include "DonorType.h"
+#include "MemberType.h"
+#include "Database.h"
+#include "DonorList.h"
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	DonorList list1;
+	list1.addDonor(Patrick, Hewes, 24, 67.0);
+	list1.createList();
+	list1.getNoOfDonors();
+	list1.isEmpty();
+	list1.searchID(80056721);
+	list1.deleteDonor(80056721);
+	list1.deleteDonor(80056721);
+	list1.printAllDonors();
+	list1.printAllDonations();
+	list1.clearList();
+	list1.~DonorList();
+	
+}
