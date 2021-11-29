702a7002ed6c34c6afd1bd17f9aacca8dc679266
NickdGreat <69378321+NickdGreat@users.noreply.github.com>
2021-11-29 02:10:14 -0800

    Update DonorList.cpp

--- Project1/DonorList.cpp
+++ Project1/DonorList.cpp
@@ -12,3 +12,24 @@
 	CSA 250
 	Project 1
 */
+#include "donorList.h";
+#include <iostream>;
+#include <string>;
+
+using namespace std;
+
+void addDonor(string firstName, string lastName, int membershipNo, int donationAmount)
+{
+	last->setNext(new Node(Donor));
+}
+
+void createList()
+{
+
+}
+
+bool isEmpty()
+{
+	return (count == 0);
+}
+// some skeletons
