8166bc1c460242586eccb5d58f44ca58cfad0a0c
HoaCTa <73952150+HoaCTa@users.noreply.github.com>
2021-11-29 22:45:12 -0800

    Create DonorListCopyFunctions.cpp

---
+++ Project1/DonorListCopyFunctions.cpp
@@ -0,0 +1,26 @@
+/*
+	Night Owls
+	Breton, Liam (leader)
+	Ta, Hoa
+	Ngo, Minh
+	Richardson, Nick
+	Riana, Ali
+	November 22, 2021
+	CSA 250
+	Project 1
+*/
+
+
+using namespace std;
+//Copy constructor
+
+
+//Copy assignment operator
+
+//Function copyCallingObjIsEmpty
+
+//Function copyObjectsSameLength
+
+//Function copyCallingObjLonger
+
+//Function copyCallingObjShorter
