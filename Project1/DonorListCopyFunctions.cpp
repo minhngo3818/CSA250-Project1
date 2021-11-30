20b6c82c56b1b158d723ba85efdc82d175ad7276
Anto Ark <tuyenngo7bh@gmail.com>
2021-11-29 22:49:56 -0800

    add part E cpp: added assignment operator copy constructor to DonorList.h

---
+++ Project1/DonorListCopyFunctions.cpp
@@ -0,0 +1,54 @@
+/*
+	Night Owls
+
+	Breton, Liam (leader)
+	Ta, Hoa
+	Ngo, Minh
+	Richardson, Nick
+	Riana, Ali
+
+	November 22, 2021
+
+	CSA 250
+	Project 1
+*/
+
+#include "DonorList.h"
+
+using namespace std;
+
+DonorList::DonorList(const DonorList& donorListToCopy)
+{
+	// Funtion body
+}
+
+DonorList& DonorList::operator=(const DonorList& donorListToCopy)
+{
+	// Funtion body
+	return *this;
+}
+
+DonorList& DonorList::copyCallingObjEmpty(const DonorList& donorListToCopy)
+{
+	// Funtion body
+	return *this;
+}
+
+DonorList& DonorList::opyObjectsSameLength(const DonorList& donorListToCopy)
+{
+	// Funtion body
+	return *this;
+}
+
+DonorList& DonorList::copyCallingObjLonger(const DonorList& donorListToCopy)
+{
+	// Funtion body
+	return *this;
+}
+
+DonorList& DonorList::copyCallingObjShorter(const DonorList& donorListToCopy)
+{
+	// Funtion body
+	return *this;
+}
+
