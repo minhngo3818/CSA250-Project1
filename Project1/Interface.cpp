24ebe198cf2d3c154a366ccd196e0d414322cf0a
Anto Ark <tuyenngo7bh@gmail.com>
2021-11-29 17:28:31 -0800

    add Interface.cpp implementation

--- Project1/Interface.cpp
+++ Project1/Interface.cpp
@@ -16,29 +16,112 @@
 #include "Interface.h"
 
 #include <iostream>
-#include <string>
+
+using namespace std;
 
 void displayMenu()
 {
-
+	cout << "*************************************************\n"
+		<< "                    MAIN MENU                    \n"
+		<< "*************************************************\n\n"
+		<< "Select one of the following:\n\n"
+		<< "    1. Add a donor\n"
+		<< "    2. Delete a donor"
+		<< "    3. Print all donors\n"
+		<< "    4. To exit\n";
 }
 
-void addDonor(DonorList&)
+void addDonor(DonorList& aDonorList)
 {
+	string inputFirstName, inputLastName;
+	int inputMembershipNo;
+	double inputAmountDonated;
 
+	cout << "\n  => Enter donor's first name: ";
+	cin >> inputFirstName;
+	cout << "\n  => Enter donor's last name: ";
+	cin >> inputLastName;
+	cout << "\n  => Enter donor's membership number: ";
+	cin >> inputMembershipNo;
+	cout << "\n  => Enter amount donated: &";
+	cin >> inputAmountDonated;
+	cout << "\n\n  => Donor has been added.\n";
+
+	aDonorList.addDonor(inputFirstName, inputLastName, inputMembershipNo, inputAmountDonated);
 }
 
-void deleteDonor(DonorList&)
+void deleteDonor(DonorList& aDonorList)
 {
+	int inputMembershipNo;
+	cout << "\n  => Enter donor's membership number: ";
+	cin >> inputMembershipNo;
+	cout << "\n\n  => Donor has been deleted.\n";
 
+	aDonorList.deleteDonor(inputMembershipNo);
 }
 
 void printAllDonors(const DonorList& aDonorList)
 {
-
+	aDonorList.printAllDonors();
 }
 
 void processSelection(DonorList& aDonorList)
 {
+	// Prompts
+	string selectionPrompt = "\n  => Enter your selection: ";
+	string continuePrompt = "\n  => Would you like to continue? (y/n) ";
+	string goodbyeMessage = "\n  => Thank you for visiting our site!\n";
+	string wrongInputMessage = "\n  => Sorry. That is not a selection.\n";
 
+	char userInput;
+	bool done = false;
+
+	while (!done)
+	{
+		// Notes: Assume user always types correct input
+
+		displayMenu();
+		cout << selectionPrompt;
+		cin >> userInput;
+		cout << endl;
+
+		if (userInput < '1' || userInput > '4')
+		{
+			done = true;
+			cout << wrongInputMessage;
+		}
+		else if (userInput >= '1' && userInput <= '4')
+		{
+			if (userInput == '4')
+			{
+				done = true;
+				cout << goodbyeMessage;
+			}
+			else
+			{
+				if (userInput == '1')
+				{
+					addDonor(aDonorList);
+				}
+				else if (userInput == '2')
+				{
+					deleteDonor(aDonorList);
+				}
+				else
+				{
+					printAllDonors(aDonorList);
+				}
+
+				cout << continuePrompt;
+				cin >> userInput;
+				cout << endl;
+
+				if (userInput != 'y')
+				{
+					done = true;
+					cout << goodbyeMessage;
+				}
+			}
+		}
+	}
 }
