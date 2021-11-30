be7146f37d9b8f89cbd74da68dfd1d85b23864d9
minhngo <tngo238@student.cccd.edu>
2021-11-29 22:58:20 -0800

    add part E stub-out implementations

--- Project1/DonorListCopyFunctions.cpp
+++ Project1/DonorListCopyFunctions.cpp
@@ -10,17 +10,49 @@
 	Project 1
 */
 
+#include "DonorList.h"
 
 using namespace std;
-//Copy constructor
 
+//Copy constructor
+DonorList::DonorList(const DonorList& donorListToCopy)
+{
+	// Funtion body
+}
 
 //Copy assignment operator
+DonorList& DonorList::operator=(const DonorList& donorListToCopy)
+{
+	// Funtion body
+	return *this;
+}
 
 //Function copyCallingObjIsEmpty
+DonorList& DonorList::copyCallingObjEmpty(const DonorList& donorListToCopy)
+{
+	// Funtion body
+	return *this;
+}
 
 //Function copyObjectsSameLength
+DonorList& DonorList::opyObjectsSameLength(const DonorList& donorListToCopy)
+{
+	// Funtion body
+	return *this;
+}
 
 //Function copyCallingObjLonger
+DonorList& DonorList::copyCallingObjLonger(const DonorList& donorListToCopy)
+{
+	// Funtion body
+	return *this;
+}
 
 //Function copyCallingObjShorter
+DonorList& DonorList::copyCallingObjShorter(const DonorList& donorListToCopy)
+{
+	// Funtion body
+	return *this;
+}
+
+
