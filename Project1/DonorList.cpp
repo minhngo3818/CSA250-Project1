b7dac2fb3c60d9b1e3b9f7641abcd23aacb982f9
NickdGreat <69378321+NickdGreat@users.noreply.github.com>
2021-11-30 23:09:27 -0800

    Update DonorList.cpp

--- Project1/DonorList.cpp
+++ Project1/DonorList.cpp
@@ -12,24 +12,78 @@
 	CSA 250
 	Project 1
 */
-#include "donorList.h";
+#include "DonorList.h";
+#include "DonorType.h";
 #include <iostream>;
 #include <string>;
+#include <iomanip>;
 
 using namespace std;
 
-void addDonor(string firstName, string lastName, int membershipNo, int donationAmount)
+DonorList::DonorList() : ptrToFirst(nullptr), ptrToLast(nullptr), count(0) {};
+
+void DonorList::addDonor(const string& newFirstName, const string& newLastName, int newMembershipNo, double newAmountDonated)
 {
-	last->setNext(new Node(Donor));
+	ptrToLast->setPtrToNext(new Node({ "", "", 0, 0.0 }, nullptr));
+	ptrToLast = ptrToLast->getPtrToNext;
 }
 
-void createList()
+bool DonorList::isEmpty() const
 {
-
+	return (count == 0); 
 }
 
-bool isEmpty()
+int DonorList::getNoOfDonors() const
 {
-	return (count == 0);
+	return count;
 }
-// some skeletons
+
+double DonorList::getTotalDonations() const
+{
+	double donationTotal = 0.0;
+	for (int i = 0; i < getData().size(); ++i)
+	{
+		i += getAmountDonated();
+		donationTotal = i;
+	}
+	return donationTotal;
+}
+
+void DonorList::deleteDonor(int membershipNo)
+{
+	if (ptrToFirst->getMembershipNo() == membershipNo)
+	{
+		Node* current = ptrToFirst;
+		ptrToFirst = ptrToFirst->getPtrToNext();
+		delete current;
+		current = nullptr;
+		--count;
+	}
+	else
+	{
+		bool found = false;	
+		Node* trailCurrent = ptrToFirst;
+		Node* current = ptrToFirst->getPtrToNext();
+		while (!found && current != nullptr)
+		{
+			if (current->getMembershipNo() == membershipNo)
+			{
+				trailCurrent->setPtrToNext(current->getPtrToNext());
+				delete current;
+				current = nullptr;
+				--count;
+				found == true;
+
+			}
+			else
+			{
+				trailCurrent = current;
+				current = current->getPtrToNext();
+			}
+		}
+		if (!found)
+		{
+			cout << "Donor is not in the list";
+		}
+	}
+}
